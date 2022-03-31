16cec00108e6e0b9711c87b44873efe02e9c0a1c
blackinitial <alhanif.blackinitial@gmail.com>
2022-03-31 16:12:09 +0700

    tambah subclass RoundHut

--- Class dan Pewarisan/class-and-inheritance.kt
+++ Class dan Pewarisan/class-and-inheritance.kt
@@ -1,10 +1,20 @@
 fun main() {
     val squareCabin = SquareCabin(6)
+    val roundHut = RoundHut(3)
 
-    println("\nSquare Cabin\n============")
-    println("Capacity: ${squareCabin.capacity}")
-    println("Material: ${squareCabin.buildingMaterial}")
-    println("Has room? ${squareCabin.hasRoom()}")
+    with(squareCabin) {
+        println("\nSquare Cabin\n============")
+        println("Material: ${buildingMaterial}")
+        println("Capacity: ${capacity}")
+        println("Has room? ${hasRoom()}")
+    }
+
+    with(roundHut) {
+        println("\nRound Hut\n=========")
+        println("Material: ${buildingMaterial}")
+        println("Capacity: ${capacity}")
+        println("Has room? ${hasRoom()}")
+    }
 }
 
 abstract class Dwelling(private var residents: Int) {
@@ -20,4 +30,9 @@ abstract class Dwelling(private var residents: Int) {
 class SquareCabin(residents: Int) : Dwelling(residents) {
     override val buildingMaterial = "Wood"
     override val capacity = 6
+}
+
+class RoundHut(residents: Int) : Dwelling(residents) {
+    override val buildingMaterial = "Straw"
+    override val capacity = 4
 }
