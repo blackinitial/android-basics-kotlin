db343c2d822d62fa654aacb1d723ee2b28ab32e1
blackinitial <alhanif.blackinitial@gmail.com>
2022-03-31 16:09:49 +0700

    tambah subclass SquareCabin

--- Class dan Pewarisan/class-and-inheritance.kt
+++ Class dan Pewarisan/class-and-inheritance.kt
@@ -1,3 +1,12 @@
+fun main() {
+    val squareCabin = SquareCabin(6)
+
+    println("\nSquare Cabin\n============")
+    println("Capacity: ${squareCabin.capacity}")
+    println("Material: ${squareCabin.buildingMaterial}")
+    println("Has room? ${squareCabin.hasRoom()}")
+}
+
 abstract class Dwelling(private var residents: Int) {
 
    abstract val buildingMaterial: String
@@ -6,4 +15,9 @@ abstract class Dwelling(private var residents: Int) {
    fun hasRoom(): Boolean {
        return residents < capacity
    }
+}
+
+class SquareCabin(residents: Int) : Dwelling(residents) {
+    override val buildingMaterial = "Wood"
+    override val capacity = 6
 }
