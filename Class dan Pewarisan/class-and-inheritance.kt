8876367855f16d8719b2addf36f03757644fce46
blackinitial <alhanif.blackinitial@gmail.com>
2022-03-31 16:07:32 +0700

    tambah classs abstrack Dwelling

---
+++ Class dan Pewarisan/class-and-inheritance.kt
@@ -0,0 +1,9 @@
+abstract class Dwelling(private var residents: Int) {
+
+   abstract val buildingMaterial: String
+   abstract val capacity: Int
+
+   fun hasRoom(): Boolean {
+       return residents < capacity
+   }
+}
