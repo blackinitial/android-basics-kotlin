ec08419bbd72b515a56af4b2ec2cfdcd7fe4ab20
blackinitial <alhanif.blackinitial@gmail.com>
2022-04-04 22:22:06 +0700

    change to string

--- List dan Loop/list-and-loop.kt
+++ List dan Loop/list-and-loop.kt
@@ -1,12 +1,20 @@
 open class Item(val name: String, val price: Int)
 
-class Noodles : Item("Noodles", 10)
+class Noodles : Item("Noodles", 10) {
+  override fun toString(): String {
+    return name
+  }
+}
 
-class Vegetables : Item("Vegetables", 5)
+class Vegetables : Item("Vegetables", 5) {
+  override fun toString(): String {
+    return name
+  }
+}
 
 fun main() {
-    val noodles = Noodles()
-    val vegetables = Vegetables()
-    println(noodles)
-    println(vegetables)
+  val noodles = Noodles()
+  val vegetables = Vegetables()
+  println(noodles)
+  println(vegetables)
 }
