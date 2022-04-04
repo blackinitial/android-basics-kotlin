985ad7338d5cc341b863dfde6af2c3367ac7ca3e
blackinitial <alhanif.blackinitial@gmail.com>
2022-04-04 22:32:47 +0700

    add order class logic

--- List dan Loop/list-and-loop.kt
+++ List dan Loop/list-and-loop.kt
@@ -1,8 +1,12 @@
 open class Item(val name: String, val price: Int)
 
-class Noodles : Item("Noodles", 10) {
+class Noodles(vararg val toppings: String) : Item("Noodles", 10) {
   override fun toString(): String {
-    return name
+    if (toppings.isEmpty()) {
+        return "$name Chef's Choice"
+    } else {
+        return name + " " + toppings.joinToString()
+    }
   }
 }
 
@@ -19,14 +23,23 @@ class Vegetables(vararg val toppings: String) : Item("Vegetables", 5) {
 class Order(val orderNumber: Int) {
    private val itemList = mutableListOf<Item>()
 
-   fun addItem(newItem: Item) {
-   }
+  fun addItem(newItem: Item) {
+    itemList.add(newItem)
+  }
 
-   fun addAll(newItems: List<Item>) {
-   }
+  fun addAll(newItems: List<Item>) {
+    itemList.addAll(newItems)
+  }
 
-   fun print() {
-   }
+  fun print() {
+    println("Order #${orderNumber}")
+    var total = 0
+    for (item in itemList) {
+      println("${item}: $${item.price}")
+      total += item.price
+    }
+    println("Total: $${total}")
+  }
 }
 
 fun main() {
